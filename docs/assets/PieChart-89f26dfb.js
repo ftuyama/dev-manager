4168adb1df98a308500a17621a57ab0678911c76
Felipe Tuyama <felipe.tuyama@thetrainline.com>
2023-02-14 23:11:33 -0300

    Update public path for assets

---
+++ docs/assets/PieChart-89f26dfb.js
@@ -0,0 +1,1 @@
+import{C as e,p as n,a as o,b as r,A as s,c as l,P as p}from"./index-f7b69f45.js";import{d as c,o as i,b as u,f as m}from"./index-d698358d.js";const g=c({__name:"PieChart",props:{chartData:null,chartOptions:null},setup(a){const t=a;return e.register(n,o,r,s,l),(f,h)=>(i(),u(m(p),{options:a.chartOptions,data:t.chartData},null,8,["options","data"]))}});export{g as default};
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const ASSET_URL = process.env.NODE_ENV === 'production' ? '/dev-manager/' : '/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: ASSET_URL,
   build: {
     chunkSizeWarningLimit: 1600,
   },
