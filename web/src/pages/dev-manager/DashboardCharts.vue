17ec608cb05c8a9e5ebfb51abeb96feda62c217f
Felipe Tuyama <felipe.tuyama@thetrainline.com>
2023-02-18 18:28:54 -0300

    Get report via API

--- web/src/pages/dev-manager/DashboardCharts.vue
+++ web/src/pages/dev-manager/DashboardCharts.vue
@@ -1,63 +1,53 @@
 <template>
-    <div class="row row-equal">
-      <div class="flex xs12 lg6 xl6">
-        <va-card v-if="radarChartData">
-          <va-card-title>
-            <h1>{{ t('dev_manager.dashboard.dev_performance') }}</h1>
-          </va-card-title>
-          <va-card-content>
-            <va-chart class="chart" :data="radarChartData" type="radar" />
-          </va-card-content>
-        </va-card>
-      </div>
-
+  <div class="row row-equal">
+    <div class="flex xs12 lg6 xl6">
+      <va-card-content>
+        <va-inner-loading :loading="loading" style="width: 100%">
+          <va-card v-if="radarChartData">
+            <va-card-title>
+              <h1>{{ t('dev_manager.dashboard.dev_performance') }}</h1>
+            </va-card-title>
+            <va-card-content>
+              <va-chart class="chart" :data="radarChartData" type="radar" />
+            </va-card-content>
+          </va-card>
+        </va-inner-loading>
+      </va-card-content>
     </div>
-  </template>
+  </div>
+</template>
 
-  <script setup lang="ts">
-    import { ref } from 'vue'
-    import { useI18n } from 'vue-i18n'
-    import VaChart from '../../components/va-charts/VaChart.vue'
+<script setup lang="ts">
+  import { onMounted, ref } from 'vue'
+  import { useI18n } from 'vue-i18n'
+  import VaChart from '../../components/va-charts/VaChart.vue'
+  import axios from 'axios'
 
-    const { t } = useI18n()
+  const { t } = useI18n()
+  const loading = ref(false)
+  let radarChartData = {
+    labels: ["Coding", "Code Review", "Versatility", "Delivery", "Charisma", "Communication"],
+    datasets: [],
+  };
 
-    const radarChartData = {
-      labels: [
-        'Eating',
-        'Drinking',
-        'Sleeping',
-        'Designing',
-        'Coding',
-        'Cycling',
-        'Running',
-      ],
-      datasets: [
-        {
-          label: 'My First dataset',
-          backgroundColor: 'rgba(220, 220, 220, 0.2)',
-          borderColor: 'rgba(220, 220, 220, 1)',
-          pointBackgroundColor: 'rgba(220, 220, 220, 1)',
-          pointBorderColor: '#fff',
-          pointHighlightFill: '#fff',
-          pointHighlightStroke: 'rgba(220, 220, 220, 1)',
-          data: [65, 59, 90, 81, 56, 55, 40],
-        },
-        {
-          label: 'My Second dataset',
-          backgroundColor: 'rgba(151, 187, 205, 0.2)',
-          borderColor: 'rgba(151, 187, 205, 1)',
-          pointBackgroundColor: 'rgba(151, 187, 205, 1)',
-          pointBorderColor: '#fff',
-          pointHighlightFill: '#fff',
-          pointHighlightStroke: 'rgba(151, 187, 205, 1)',
-          data: [28, 48, 40, 19, 96, 27, 100],
-        },
-      ],
-    };
-  </script>
+  onMounted(() => {
+    loadRadarChartData()
+  })
 
-  <style scoped>
-    .chart {
-      height: 400px;
-    }
-  </style>
+  async function loadRadarChartData() {
+    loading.value = true
+    const { data } = await axios.get('/report.json')
+    loading.value = false
+    radarChartData['datasets'] = data;
+  }
+</script>
+
+<style scoped>
+  .chart {
+    height: 400px;
+  }
+
+  .inner-loading {
+    height: 100%;
+  }
+</style>
